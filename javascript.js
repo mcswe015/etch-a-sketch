651d4d921afcb1c6b173919999fb97c9023ec3f1
mcswe015 <tessammcsweeney@gmail.com>
2023-11-29 20:28:34 -0500

    made grid using CSS, JS

---
+++ javascript.js
@@ -0,0 +1,21 @@
+let parent = document.querySelector("#parent");
+
+function createDivs(){
+    for(let j=1;j<=16;j++){
+    let row=document.createElement("div");
+    row.classList.add("row");
+    parent.appendChild(row);
+        for(let i=1;i<=16;i++){
+            let div = document.createElement("div");
+            div.classList.add("boxes");
+            row.appendChild(div);
+        
+        }
+    }
+}
+
+window.addEventListener('load',()=>{
+    createDivs();
+})
+
+
