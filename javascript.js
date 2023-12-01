40f74557d29a64b25b2755bdd4a55a58d5d7ae5b
mcswe015 <tessammcsweeney@gmail.com>
2023-11-30 19:29:21 -0500

    started adding in input box and button

--- javascript.js
+++ javascript.js
@@ -1,10 +1,28 @@
 let parent = document.querySelector("#parent");
 
+let buttonDiv = document.createElement("div");
+
+let input = document.createElement("input");
+
+buttonDiv.appendChild(input);
+
+let button = document.createElement("button");
+
+buttonDiv.appendChild(button);
+
+parent.appendChild(buttonDiv);
+
+let boxDiv=document.createElement("div");
+boxDiv.classList.add("boxdiv");
+parent.appendChild(boxDiv);
+
+
+
 function createDivs(){
     for(let j=1;j<=16;j++){
     let row=document.createElement("div");
     row.classList.add("row");
-    parent.appendChild(row);
+    boxDiv.appendChild(row);
         for(let i=1;i<=16;i++){
             let div = document.createElement("div");
             div.classList.add("boxes");
